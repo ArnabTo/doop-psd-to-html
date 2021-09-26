5273b5b7906236f24e7f095f26d9334f8bba1b2b
Arnab Saha <72820989+ArnabTo@users.noreply.github.com>
2021-09-26 14:19:34 +0600

    psd-to-html

---
+++ app/app.js
@@ -0,0 +1,11 @@
+let menuDiv= document.getElementsByClassName("men_items col-7");
+let menuIcon= document.getElementsByClassName("n_icon");
+
+// menuIcon[0].addEventListener("click", function(){
+//     menuDiv.classList.toggle("show");
+// })
+
+
+function clickFunc(){
+    menuDiv[0].classList.toggle("show")
+}
---
+++ app/setting.js
@@ -0,0 +1,6 @@
+let menuIcon= document.getElementsByClassName("n_icon");
+let menuDiv= document.getElementsByClassName("men_items"); 
+
+function toggleF(){
+    menuDiv[0].classList.toggle("show")
+}
